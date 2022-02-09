82256f26b51c9a1e3c741cb4eca842e73004b69b
ambiti0n <fabulousfresh010@gmail.com>
2022-02-09 03:21:19 +0100

    register user functionality implemented

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { from, Observable } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
+
+import { UserEntity } from '../models/user.entity';
+import { User } from '../models/user.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+  hashPassword(password: string): Observable<string> {
+    return from(bcrypt.hash(password, 12));
+  }
+
+  registerAccount(user: User): Observable<User> {
+    const { firstName, lastName, email, password } = user;
+
+    return this.hashPassword(password).pipe(
+      switchMap(() => {
+        return this.hashPassword(password).pipe(
+          switchMap((hashedPassword: string) => {
+            return from(
+              this.userRepository.save({
+                firstName,
+                lastName,
+                email,
+                password: hashedPassword,
+              }),
+            ).pipe(
+              map((user: User) => {
+                delete user.password;
+                return user;
+              }),
+            );
+          }),
+        );
+      }),
+    );
+  }
+}
